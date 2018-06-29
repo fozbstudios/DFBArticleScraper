d838d4aa6a746c9b268425ffcc98d3dab8167c3f
Nick Kirkpatrick <nicholasmichaelkirkpatrick@gmail.com>
2018-06-29 10:56:53 -0500

    modified settings in line with my gist

--- disneyFoodBlogArticleScraper/disneyFoodBlogArticleScraper/settings.py
+++ disneyFoodBlogArticleScraper/disneyFoodBlogArticleScraper/settings.py
@@ -19,24 +19,24 @@ NEWSPIDER_MODULE = 'disneyFoodBlogArticleScraper.spiders'
 #USER_AGENT = 'disneyFoodBlogArticleScraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
@@ -47,37 +47,38 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'disneyFoodBlogArticleScraper.middlewares.DisneyfoodblogarticlescraperSpiderMiddleware': 543,
+#    'disneyFoodBlogArticleScraper.middlewares.disneyFoodBlogArticleScraperSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'disneyFoodBlogArticleScraper.middlewares.DisneyfoodblogarticlescraperDownloaderMiddleware': 543,
+#    'disneyFoodBlogArticleScraper.middlewares.disneyFoodBlogArticleScraperDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+EXTENSIONS = {
+    'scrapy.extensions.telnet.TelnetConsole': None,
+}
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'disneyFoodBlogArticleScraper.pipelines.DisneyfoodblogarticlescraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'disneyFoodBlogArticleScraper.pipelines.disneyFoodBlogArticleScraperPipeline': 1,
+}
 
+LOG_LEVEL = 'INFO'
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 1
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 20
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 15
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
