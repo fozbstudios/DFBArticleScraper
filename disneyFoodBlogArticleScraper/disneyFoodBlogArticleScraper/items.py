cd0c313aa5e080cc3acbe5b51248a479d12dcd61
Nick Kirkpatrick <nicholasmichaelkirkpatrick@gmail.com>
2018-06-29 11:38:05 -0500

    setup item class

--- disneyFoodBlogArticleScraper/disneyFoodBlogArticleScraper/items.py
+++ disneyFoodBlogArticleScraper/disneyFoodBlogArticleScraper/items.py
@@ -10,5 +10,6 @@ import scrapy
 
 class DisneyfoodblogarticlescraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
     pass
